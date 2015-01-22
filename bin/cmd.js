6dbafdb8ade1d50bc1ff909e0972a61430caa88c
Scott Corgan <scottcorgan@gmail.com>
2015-01-22 11:47:51 -0800

    bug: stdout/err all the things. Fixes #1

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,20 +5,26 @@ var spawn = require('cross-spawn');
 
 console.log('\nRemoving node_modules directory ...');
 
-spawn('rm', ['-rf', 'node_modules'])
+spawn('rm', ['-rf', 'node_modules'], { stdio: ['pipe', 'pipe', process.stderr] })
   .on('close', function () {
   
     console.log('Cleaning NPM cache ..');
     
-    spawn('npm', ['cache', 'clean'])
+    spawn('npm', ['cache', 'clean'], { stdio: ['pipe', 'pipe', process.stderr] })
       .on('close', function () {
         
         console.log('Installing dependencies ...');
         
-        spawn('npm', ['install'])
-          .on('close', function () {
+        spawn('npm', ['install'], { stdio: ['pipe', 'pipe', process.stderr] })
+          .on('close', function (err) {
             
-            console.log('Done!');
+            var msg = 'Done!';
+            
+            if (err) {
+              msg = 'Done, but with errors.';
+            }
+            
+            console.log('\n' + msg + '\n');
           });
       });
   });
