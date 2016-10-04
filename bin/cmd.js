4155615ea7c83e60829602b77c48e60fe9654295
Eugen Istoc <eugenistoc@gmail.com>
2016-10-04 11:46:29 -0400

    Use rimraf
    Use rimraf for cross platform folder deletion.

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-
 var spawn = require('cross-spawn');
+var rimraf = require('rimraf');
 
 console.log('\nRemoving node_modules directory ...');
 
-spawn('rm', ['-rf', 'node_modules'], { stdio: ['pipe', 'pipe', process.stderr] })
-  .on('close', function () {
-  
+rimraf('node_modules', {}, function () {
     console.log('Cleaning NPM cache ..');
     
     spawn('npm', ['cache', 'clean'], { stdio: ['pipe', 'pipe', process.stderr] })
