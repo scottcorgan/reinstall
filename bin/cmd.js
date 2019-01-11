902c12006baae3024881721da87ba5425f27f3fa
Loay <loay@users.noreply.github.com>
2019-01-10 21:09:22 -0500

    Add rimraf of  package-lock command
    package-lock can create lots of conflicts, so it is better to have a clean version when reinstalling node modules

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,26 +3,30 @@
 var spawn = require('cross-spawn');
 var rimraf = require('rimraf');
 
-console.log('\nRemoving node_modules directory ...');
+console.log('\nRemoving package-lock.json file ...');
 
-rimraf('node_modules', {}, function () {
-    console.log('Cleaning NPM cache ..');
-    
-    spawn('npm', ['cache', 'clean', '--force'], { stdio: ['pipe', 'pipe', process.stderr] })
-      .on('close', function () {
-        
-        console.log('Installing dependencies ...');
-        
-        spawn('npm', ['install'], { stdio: ['pipe', 'pipe', process.stderr] })
-          .on('close', function (err) {
-            
-            var msg = 'Done!';
-            
-            if (err) {
-              msg = 'Done, but with errors.';
-            }
-            
-            console.log('\n' + msg + '\n');
-          });
-      });
-  });
+rimraf('package-lock.json', {}, function () {
+  console.log('\nRemoving node_modules directory ...');
+
+  rimraf('node_modules', {}, function () {
+      console.log('Cleaning NPM cache ..');
+
+      spawn('npm', ['cache', 'clean', '--force'], { stdio: ['pipe', 'pipe', process.stderr] })
+        .on('close', function () {
+
+          console.log('Installing dependencies ...');
+
+          spawn('npm', ['install'], { stdio: ['pipe', 'pipe', process.stderr] })
+            .on('close', function (err) {
+
+              var msg = 'Done!';
+
+              if (err) {
+                msg = 'Done, but with errors.';
+              }
+
+              console.log('\n' + msg + '\n');
+            });
+        });
+    });
+});
